0826486aece99795f0c2bfd77fafb6f1d99a26aa
duytt3 <duytt3@vng.com.vn>
2024-07-19 14:08:01 +0700

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
   return (
-    <div className="text-center text-3xl font-bold italic text-red-300 underline">
-      Hello world
-    </div>
+    <>
+      <header className="fixed z-[500] h-16 w-screen transition-all duration-300 text-white bg-black/50 backdrop-blur-xl">
+        <div className="container mx-auto flex h-full flex-row items-center justify-between">
+          <div className="flex flex-1">
+            <img
+              className="mr-8 w-16 object-contain"
+              src="/assets/netflix.png"
+              alt="logo"
+            />
+            <div className="flex items-center gap-8">
+              <a href="#">Phim</a>
+              <a href="#">Truyền hình</a>
+            </div>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+        </div>
+      </header>
+      <div className='w-full h-[500px] bg-blue-900'></div>
+      <div className='w-full h-[500px] bg-white'></div>
+      <div className='w-full h-[500px] bg-red-400'></div>
+      <div className='w-full h-[500px] bg-green-500'></div>
+    </>
   );
 }
 
